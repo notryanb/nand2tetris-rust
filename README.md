f5f5d26410f949d29414d6977f9440dd882bd378
notryanb@gmail.com <notryanb@gmail.com>
2019-05-27 18:04:12 -0400

    Change everything into doc tests

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This project is an attempt to work through the exercises found in the book,
 [The Elements of Computing Systems - Building a Modern Computer from First Principles](https://www.nand2tetris.org/).
 While the website includes an HDL program and other goodies,
 I thought it might be interesting to see how this might work out in code as I work through the real HDL and rust library simultaneously.
+
+The NAND Gate is implemented first with a truth table, and then all other gates are built sucessively from it. 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+/// Represents the canonical Logic units in a truth table for boolean logic.
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum LogicValue {
     False,
@@ -5,7 +6,23 @@ pub enum LogicValue {
 }
 
 
-// NAND Gate
+/// NAND Gate function (not-and). Takes two logic values
+/// and outputs a value based on the following table.
+/// 
+/// | a | b | out |
+/// |---|---|-----|
+/// | 0 | 0 | 1   |
+/// | 0 | 1 | 1   |
+/// | 1 | 0 | 1   |
+/// | 1 | 1 | 0   |
+///
+/// ```
+/// # use nand2tetris_rust::{nand, LogicValue};
+/// assert_eq!(nand(LogicValue::False, LogicValue::False), LogicValue::True);
+/// assert_eq!(nand(LogicValue::False, LogicValue::True), LogicValue::True);
+/// assert_eq!(nand(LogicValue::True, LogicValue::False), LogicValue::True);
+/// assert_eq!(nand(LogicValue::True, LogicValue::True), LogicValue::False);
+/// ```
 pub fn nand(a: LogicValue, b: LogicValue) -> LogicValue {
     match (a, b) {
         (LogicValue::True, LogicValue::True) => LogicValue::False,
@@ -15,21 +32,86 @@ pub fn nand(a: LogicValue, b: LogicValue) -> LogicValue {
     }
 }
 
+/// Not Gate function. Takes one logic value
+/// and outputs a value based on the following table.
+/// 
+/// | a | out |
+/// |---|-----|
+/// | 0 | 1   |
+/// | 1 | 0   |
+///
+/// ```
+/// # use nand2tetris_rust::{not, LogicValue};
+/// assert_eq!(not(LogicValue::False), LogicValue::True);
+/// assert_eq!(not(LogicValue::True), LogicValue::False);
+/// ```
 pub fn not(a: LogicValue) -> LogicValue {
     nand(a, a)
 }
 
+/// AND Gate function. Takes two logic values
+/// and outputs a value based on the following table.
+/// 
+/// | a | b | out |
+/// |---|---|-----|
+/// | 0 | 0 | 0   |
+/// | 0 | 1 | 0   |
+/// | 1 | 0 | 0   |
+/// | 1 | 1 | 1   |
+///
+/// ```
+/// # use nand2tetris_rust::{and, LogicValue};
+/// assert_eq!(and(LogicValue::False, LogicValue::False), LogicValue::False);
+/// assert_eq!(and(LogicValue::False, LogicValue::True), LogicValue::False);
+/// assert_eq!(and(LogicValue::True, LogicValue::False), LogicValue::False);
+/// assert_eq!(and(LogicValue::True, LogicValue::True), LogicValue::True);
+/// ```
 pub fn and(a: LogicValue, b: LogicValue) -> LogicValue {
     let nand_result = nand(a, b);
     not(nand_result)
 }
 
+
+/// OR Gate function. Takes two logic values
+/// and outputs a value based on the following table.
+/// 
+/// | a | b | out |
+/// |---|---|-----|
+/// | 0 | 0 | 0   |
+/// | 0 | 1 | 1   |
+/// | 1 | 0 | 1   |
+/// | 1 | 1 | 1   |
+///
+/// ```
+/// # use nand2tetris_rust::{or, LogicValue};
+/// assert_eq!(or(LogicValue::False, LogicValue::False), LogicValue::False);
+/// assert_eq!(or(LogicValue::False, LogicValue::True), LogicValue::True);
+/// assert_eq!(or(LogicValue::True, LogicValue::False), LogicValue::True);
+/// assert_eq!(or(LogicValue::True, LogicValue::True), LogicValue::True);
+/// ```
 pub fn or(a: LogicValue, b: LogicValue) -> LogicValue {
     let nand_result_1 = nand(a, a);
     let nand_result_2 = nand(b, b);
     nand(nand_result_1, nand_result_2)
 }
 
+/// XOR Gate function (Exclusive OR). Takes two logic values
+/// and outputs a value based on the following table.
+/// 
+/// | a | b | out |
+/// |---|---|-----|
+/// | 0 | 0 | 0   |
+/// | 0 | 1 | 1   |
+/// | 1 | 0 | 1   |
+/// | 1 | 1 | 0   |
+///
+/// ```
+/// # use nand2tetris_rust::{xor, LogicValue};
+/// assert_eq!(xor(LogicValue::False, LogicValue::False), LogicValue::False);
+/// assert_eq!(xor(LogicValue::False, LogicValue::True), LogicValue::True);
+/// assert_eq!(xor(LogicValue::True, LogicValue::False), LogicValue::True);
+/// assert_eq!(xor(LogicValue::True, LogicValue::True), LogicValue::False);
+/// ```
 pub fn xor(a: LogicValue, b: LogicValue) -> LogicValue {
     let not_a = not(a);
     let not_b = not(b);
@@ -37,47 +119,4 @@ pub fn xor(a: LogicValue, b: LogicValue) -> LogicValue {
     let nand_result_2 = nand(not_a, b);
     nand(nand_result_1, nand_result_2)
 
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-   
-        #[test]
-    fn nand_test() {
-        assert_eq!(nand(LogicValue::True, LogicValue::True), LogicValue::False);
-        assert_eq!(nand(LogicValue::False, LogicValue::True), LogicValue::True);
-        assert_eq!(nand(LogicValue::True, LogicValue::False), LogicValue::True);
-        assert_eq!(nand(LogicValue::False, LogicValue::False), LogicValue::True);
-    }
-    
-    #[test]
-    fn not_test() {
-        assert_eq!(not(LogicValue::True), LogicValue::False);
-        assert_eq!(not(LogicValue::False), LogicValue::True);
-    }
-    
-    #[test]
-    fn and_test() {
-        assert_eq!(and(LogicValue::True, LogicValue::True), LogicValue::True);
-        assert_eq!(and(LogicValue::False, LogicValue::True), LogicValue::False);
-        assert_eq!(and(LogicValue::True, LogicValue::False), LogicValue::False);
-        assert_eq!(and(LogicValue::False, LogicValue::False), LogicValue::False);
-    }
-    
-    #[test]
-    fn or_test() {
-        assert_eq!(or(LogicValue::True, LogicValue::True), LogicValue::True);
-        assert_eq!(or(LogicValue::False, LogicValue::True), LogicValue::True);
-        assert_eq!(or(LogicValue::True, LogicValue::False), LogicValue::True);
-        assert_eq!(or(LogicValue::False, LogicValue::False), LogicValue::False);
-    }
-    
-    #[test]
-    fn xor_test() {
-        assert_eq!(xor(LogicValue::True, LogicValue::True), LogicValue::False);
-        assert_eq!(xor(LogicValue::False, LogicValue::True), LogicValue::True);
-        assert_eq!(xor(LogicValue::True, LogicValue::False), LogicValue::True);
-        assert_eq!(xor(LogicValue::False, LogicValue::False), LogicValue::False);
-    }
 }
